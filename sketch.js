826489594e24ba2d353486f951ee514c2bae13a3
Ayushi Shah <as17726@nyu.edu>
2023-10-08 18:54:27 -0400

    Attempt 01

--- sketch.js
+++ sketch.js
@@ -1,6 +1,31 @@
+let dataObject;
+let data;
+let latitudeMin; 
+let latitudeMax; 
+let longitudeMin; 
+let longitudeMax; 
+
+function preload() {
+  dataObject = loadJSON("./Tree-Census-2015.json");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  data = Object.values(dataObject);
+  for (let i = 0; i < data.length; i++) {
+    let latitude = data[i].latitude;
+    let longitude = data[i].longitude;
+
+    latitudeMin = min(latitudeMin, price);
+    latitudeMax = max(latitudeMax, price);
+    longitudeMin = min(longitudeMin, points);
+    longitudeMax = max(longitudeMax, points);
+  }
 }
 
 function draw() {
+  background(0);
+  for (let i = 0; i < data.length; i++) {
+    
+  }
 }
